d7539c86af3f4509b0561c4c20f544e70cbf80b7
isaac.florence <isaac.florence@phe.gov.uk>
2021-03-20 22:35:22 +0000

    Create showflow.R

---
+++ R/showflow.R
@@ -0,0 +1,43 @@
+#' @name showflow
+#'
+#' @description
+#' Parse repository, document flows, and optionally show flows
+#' Uses special code annotation to document flows
+#'
+#' @param dir Directory/top-level of repository
+#'
+#' @export
+showflow <- function(dir = "."){
+
+  dir <- normalizePath(dir, mustWork = T)
+
+  files <- list.files(dir, "\\.R$", recursive = T)
+
+  blocks <- lapply(files, shfl_blocks)
+
+}
+
+
+
+
+#' @name
+shfl_blocks <- function(file = NULL){
+
+  contents <- readr::read_lines(file)
+
+  comments <- contents[grep("^#'", contents)]
+
+  com_head_ref <- grep("@name", comments)
+
+  blocks <- list()
+
+  if(length(com_head_ref) == 1)
+
+  for(i in seq_along(com_head_ref)){
+    blocks <- c(blocks,
+                comments[
+                  com_head_ref[i]:com_head_ref[i+1]-1
+                  ])
+  }
+
+}
