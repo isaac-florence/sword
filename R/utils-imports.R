ce146f3d76e034cf3a71a931c32eb0188cb1241a
Isaac Florence <30433879+isaac-florence@users.noreply.github.com>
2021-03-24 23:51:50 +0000

    remove Rd

--- R/utils-imports.R
+++ R/utils-imports.R
@@ -3,9 +3,8 @@
 #' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
 #'
 #' @name %>%
-#' @rdname pipe
+#' @noRd
 #' @keywords internal
-#' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
@@ -16,9 +15,8 @@ NULL
 #'
 #' @details The tee pipe works like %>%, except the return value is lhs itself, and not the result of rhs function/expression.
 #' @name %T>%
-#' @rdname pipe
+#' @noRd
 #' @keywords internal
-#' @export
 #' @importFrom magrittr %T>%
 #' @usage lhs \%T>\% rhs
 NULL
