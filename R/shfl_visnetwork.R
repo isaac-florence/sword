17dbdecde131aad8f06713f1361bbce9afeb8ba4
Isaac Florence <30433879+isaac-florence@users.noreply.github.com>
2021-03-26 00:48:07 +0000

    Create shfl_visnetwork.R

---
+++ R/shfl_visnetwork.R
@@ -0,0 +1,31 @@
+
+get_visnetwork <- function(nw_elements = NULL){
+
+  edges <- nw_elements$dependencies %>%
+    dplyr::mutate(dashes = dplyr::if_else(dep_type == "uses", FALSE, TRUE))
+
+  nodes <- nw_elements$attributes %>%
+    dplyr::mutate(shape = "icon",
+                  icon.code = dplyr::case_when(
+                    type == "load"    ~ "f1c0",
+                    type == "save"    ~ "f0ab",
+                    type == "process" ~ "f111",
+                    type == "setup"   ~ "f0c8"))
+
+  visnetwork <- visNetwork::visNetwork(
+    nodes, edges,
+    main = list(text = paste(nw_elements$flows$title),
+                style = "font-family:Georgia;font-weight:bold;font-size:20px;text-align:center;"),
+    footer = list(text = paste(nw_elements$flows$description),
+                  style = "font-family:serif;font-size:16px;text-align:left;"),
+    width = "800px", height = "400px"
+  ) %>%
+    visNetwork::visEdges(
+      arrows = "to", width = 2,
+      smooth = list(type = "cubicBezier",
+                    forceDirection = "horizontal")
+    ) %>%
+    visNetwork::visHierarchicalLayout(direction = "LR", sortMethod = "directed") %>%
+    vizNetwork::visOptions(highlightNearest = FALSE) %>%
+    visNetwork::addFontAwesome()
+}
